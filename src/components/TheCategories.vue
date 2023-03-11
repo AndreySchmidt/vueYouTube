ec445e53c27ad9ee9501cbb8de80d0fdd186d7a5
AndreySchmidt <ringnull@gmail.com>
2023-03-11 17:14:10 +0300

    markup to tml

---
+++ src/components/TheCategories.vue
@@ -0,0 +1,142 @@
+<template>
+  <section
+    class="xl:pl-64 md:pl-24 pt-14 fixed bg-white bg-opacity-90 z-10 w-full"
+  >
+    <div class="border-t border-b px-4 max-w-screen-2xl m-auto">
+      <div class="py-3 flex space-x-3 overflow-auto whitespace-nowrap">
+        <a
+          href=""
+          class="text-sm px-3 py-1 inline-block rounded-full bg-gray-100 border border-gray-300 hover:bg-gray-200 transition"
+        >
+          All
+        </a>
+        <a
+          href=""
+          class="text-sm px-3 py-1 inline-block rounded-full bg-gray-600 border border-gray-700 hover:bg-gray-500 text-white transition"
+        >
+          Trucks
+        </a>
+        <a
+          href=""
+          class="text-sm px-3 py-1 inline-block rounded-full bg-gray-100 border border-gray-300 hover:bg-gray-200 transition"
+        >
+          Machines
+        </a>
+        <a
+          href=""
+          class="text-sm px-3 py-1 inline-block rounded-full bg-gray-100 border border-gray-300 hover:bg-gray-200 transition"
+        >
+          Engines
+        </a>
+        <a
+          href=""
+          class="text-sm px-3 py-1 inline-block rounded-full bg-gray-100 border border-gray-300 hover:bg-gray-200 transition"
+        >
+          Snow
+        </a>
+        <a
+          href=""
+          class="text-sm px-3 py-1 inline-block rounded-full bg-gray-100 border border-gray-300 hover:bg-gray-200 transition"
+        >
+          Ships
+        </a>
+        <a
+          href=""
+          class="text-sm px-3 py-1 inline-block rounded-full bg-gray-100 border border-gray-300 hover:bg-gray-200 transition"
+        >
+          Roads
+        </a>
+        <a
+          href=""
+          class="text-sm px-3 py-1 inline-block rounded-full bg-gray-100 border border-gray-300 hover:bg-gray-200 transition"
+        >
+          Tanks
+        </a>
+        <a
+          href=""
+          class="text-sm px-3 py-1 inline-block rounded-full bg-gray-100 border border-gray-300 hover:bg-gray-200 transition"
+        >
+          Buildings
+        </a>
+        <a
+          href=""
+          class="text-sm px-3 py-1 inline-block rounded-full bg-gray-100 border border-gray-300 hover:bg-gray-200 transition"
+        >
+          Tractors
+        </a>
+        <a
+          href=""
+          class="text-sm px-3 py-1 inline-block rounded-full bg-gray-100 border border-gray-300 hover:bg-gray-200 transition"
+        >
+          Football
+        </a>
+        <a
+          href=""
+          class="text-sm px-3 py-1 inline-block rounded-full bg-gray-100 border border-gray-300 hover:bg-gray-200 transition"
+        >
+          All
+        </a>
+        <a
+          href=""
+          class="text-sm px-3 py-1 inline-block rounded-full bg-gray-100 border border-gray-300 hover:bg-gray-200 transition"
+        >
+          Trucks
+        </a>
+        <a
+          href=""
+          class="text-sm px-3 py-1 inline-block rounded-full bg-gray-100 border border-gray-300 hover:bg-gray-200 transition"
+        >
+          Machines
+        </a>
+        <a
+          href=""
+          class="text-sm px-3 py-1 inline-block rounded-full bg-gray-100 border border-gray-300 hover:bg-gray-200 transition"
+        >
+          Engines
+        </a>
+        <a
+          href=""
+          class="text-sm px-3 py-1 inline-block rounded-full bg-gray-100 border border-gray-300 hover:bg-gray-200 transition"
+        >
+          Snow
+        </a>
+        <a
+          href=""
+          class="text-sm px-3 py-1 inline-block rounded-full bg-gray-100 border border-gray-300 hover:bg-gray-200 transition"
+        >
+          Ships
+        </a>
+        <a
+          href=""
+          class="text-sm px-3 py-1 inline-block rounded-full bg-gray-100 border border-gray-300 hover:bg-gray-200 transition"
+        >
+          Roads
+        </a>
+        <a
+          href=""
+          class="text-sm px-3 py-1 inline-block rounded-full bg-gray-100 border border-gray-300 hover:bg-gray-200 transition"
+        >
+          Tanks
+        </a>
+        <a
+          href=""
+          class="text-sm px-3 py-1 inline-block rounded-full bg-gray-100 border border-gray-300 hover:bg-gray-200 transition"
+        >
+          Buildings
+        </a>
+        <a
+          href=""
+          class="text-sm px-3 py-1 inline-block rounded-full bg-gray-100 border border-gray-300 hover:bg-gray-200 transition"
+        >
+          Tractors
+        </a>
+        <a
+          href=""
+          class="text-sm px-3 py-1 inline-block rounded-full bg-gray-100 border border-gray-300 hover:bg-gray-200 transition"
+        >
+          Football
+        </a>
+      </div>
+    </div>
+  </section>
+</template>
---
+++ src/components/TheSidebarSmall.vue
@@ -0,0 +1,130 @@
+<template>
+  <aside
+    class="hidden md:block min-h-screen overflow-auto fixed top-0 pt-14 bg-white z-20"
+  >
+    <section>
+      <ul>
+        <li>
+          <a
+            href=""
+            class="flex flex-col items-center px-2 py-5 text-red-500 hover:bg-gray-100"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6 mb-1.5"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+              />
+            </svg>
+            <span class="text-xs"> Home </span>
+          </a>
+        </li>
+        <li>
+          <a
+            href=""
+            class="flex flex-col items-center px-2 py-5 hover:bg-gray-100"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6 mb-1.5"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M15.362 5.214A8.252 8.252 0 0112 21 8.25 8.25 0 016.038 7.048 8.287 8.287 0 009 9.6a8.983 8.983 0 013.361-6.867 8.21 8.21 0 003 2.48z"
+              />
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 18a3.75 3.75 0 00.495-7.467 5.99 5.99 0 00-1.925 3.546 5.974 5.974 0 01-2.133-1A3.75 3.75 0 0012 18z"
+              />
+            </svg>
+            <span class="text-xs"> Trending </span>
+          </a>
+        </li>
+        <li>
+          <a
+            href=""
+            class="flex flex-col items-center px-2 py-5 hover:bg-gray-100"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6 mb-1.5"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z"
+              />
+            </svg>
+            <span class="text-xs"> Subscriptions </span>
+          </a>
+        </li>
+        <li>
+          <a
+            href=""
+            class="flex flex-col items-center px-2 py-5 hover:bg-gray-100"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6 mb-1.5"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75"
+              />
+            </svg>
+            <span class="text-xs"> Library </span>
+          </a>
+        </li>
+        <li>
+          <a
+            href=""
+            class="flex flex-col items-center px-2 py-5 hover:bg-gray-100"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6 mb-1.5"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+            <span class="text-xs"> History </span>
+          </a>
+        </li>
+      </ul>
+    </section>
+  </aside>
+</template>
