1d348aeee3e9efed806b0eb4e2ec10fe7f4efb1e
AndreySchmidt <ringnull@gmail.com>
2023-03-11 18:17:13 +0300

    category, video item

--- src/components/TheCategories.vue
+++ src/components/TheCategories.vue
@@ -5,138 +5,35 @@
     <div class="border-t border-b px-4 max-w-screen-2xl m-auto">
       <div class="py-3 flex space-x-3 overflow-auto whitespace-nowrap">
         <a
+          v-for="category in categories"
+          :key="category"
           href=""
           class="text-sm px-3 py-1 inline-block rounded-full bg-gray-100 border border-gray-300 hover:bg-gray-200 transition"
         >
-          All
-        </a>
-        <a
-          href=""
-          class="text-sm px-3 py-1 inline-block rounded-full bg-gray-600 border border-gray-700 hover:bg-gray-500 text-white transition"
-        >
-          Trucks
-        </a>
-        <a
-          href=""
-          class="text-sm px-3 py-1 inline-block rounded-full bg-gray-100 border border-gray-300 hover:bg-gray-200 transition"
-        >
-          Machines
-        </a>
-        <a
-          href=""
-          class="text-sm px-3 py-1 inline-block rounded-full bg-gray-100 border border-gray-300 hover:bg-gray-200 transition"
-        >
-          Engines
-        </a>
-        <a
-          href=""
-          class="text-sm px-3 py-1 inline-block rounded-full bg-gray-100 border border-gray-300 hover:bg-gray-200 transition"
-        >
-          Snow
-        </a>
-        <a
-          href=""
-          class="text-sm px-3 py-1 inline-block rounded-full bg-gray-100 border border-gray-300 hover:bg-gray-200 transition"
-        >
-          Ships
-        </a>
-        <a
-          href=""
-          class="text-sm px-3 py-1 inline-block rounded-full bg-gray-100 border border-gray-300 hover:bg-gray-200 transition"
-        >
-          Roads
-        </a>
-        <a
-          href=""
-          class="text-sm px-3 py-1 inline-block rounded-full bg-gray-100 border border-gray-300 hover:bg-gray-200 transition"
-        >
-          Tanks
-        </a>
-        <a
-          href=""
-          class="text-sm px-3 py-1 inline-block rounded-full bg-gray-100 border border-gray-300 hover:bg-gray-200 transition"
-        >
-          Buildings
-        </a>
-        <a
-          href=""
-          class="text-sm px-3 py-1 inline-block rounded-full bg-gray-100 border border-gray-300 hover:bg-gray-200 transition"
-        >
-          Tractors
-        </a>
-        <a
-          href=""
-          class="text-sm px-3 py-1 inline-block rounded-full bg-gray-100 border border-gray-300 hover:bg-gray-200 transition"
-        >
-          Football
-        </a>
-        <a
-          href=""
-          class="text-sm px-3 py-1 inline-block rounded-full bg-gray-100 border border-gray-300 hover:bg-gray-200 transition"
-        >
-          All
-        </a>
-        <a
-          href=""
-          class="text-sm px-3 py-1 inline-block rounded-full bg-gray-100 border border-gray-300 hover:bg-gray-200 transition"
-        >
-          Trucks
-        </a>
-        <a
-          href=""
-          class="text-sm px-3 py-1 inline-block rounded-full bg-gray-100 border border-gray-300 hover:bg-gray-200 transition"
-        >
-          Machines
-        </a>
-        <a
-          href=""
-          class="text-sm px-3 py-1 inline-block rounded-full bg-gray-100 border border-gray-300 hover:bg-gray-200 transition"
-        >
-          Engines
-        </a>
-        <a
-          href=""
-          class="text-sm px-3 py-1 inline-block rounded-full bg-gray-100 border border-gray-300 hover:bg-gray-200 transition"
-        >
-          Snow
-        </a>
-        <a
-          href=""
-          class="text-sm px-3 py-1 inline-block rounded-full bg-gray-100 border border-gray-300 hover:bg-gray-200 transition"
-        >
-          Ships
-        </a>
-        <a
-          href=""
-          class="text-sm px-3 py-1 inline-block rounded-full bg-gray-100 border border-gray-300 hover:bg-gray-200 transition"
-        >
-          Roads
-        </a>
-        <a
-          href=""
-          class="text-sm px-3 py-1 inline-block rounded-full bg-gray-100 border border-gray-300 hover:bg-gray-200 transition"
-        >
-          Tanks
-        </a>
-        <a
-          href=""
-          class="text-sm px-3 py-1 inline-block rounded-full bg-gray-100 border border-gray-300 hover:bg-gray-200 transition"
-        >
-          Buildings
-        </a>
-        <a
-          href=""
-          class="text-sm px-3 py-1 inline-block rounded-full bg-gray-100 border border-gray-300 hover:bg-gray-200 transition"
-        >
-          Tractors
-        </a>
-        <a
-          href=""
-          class="text-sm px-3 py-1 inline-block rounded-full bg-gray-100 border border-gray-300 hover:bg-gray-200 transition"
-        >
-          Football
+          {{ category }}
         </a>
       </div>
     </div>
   </section>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      categories: [
+        "All",
+        "Trucks",
+        "Machines",
+        "Engines",
+        "Snow",
+        "Ships",
+        "Roads",
+        "Tanks",
+        "Buildings",
+        "Tractors",
+        "Football",
+      ],
+    };
+  },
+};
+</script>
