d1f3e7de39a2c2c404e2c75e550439a62acd80f2
AndreySchmidt <ringnull@gmail.com>
2023-03-14 11:12:49 +0300

    TheHeader to components

---
+++ src/components/TheSearch.vue
@@ -0,0 +1,39 @@
+<template>
+  <div
+    class="hidden sm:flex flex-1 justify-end pl-8 md:pl-12 md:px-8 lg:px-0 items-center lg:w-1/2 max-w-md"
+  >
+    <div class="flex w-full h-full p-2.5">
+      <TheSearchInput />
+      <TheSearchButton />
+    </div>
+    <button class="ml-4 focus:outline-none">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-5 h-5"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M12 18.75a6 6 0 006-6v-1.5m-6 7.5a6 6 0 01-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 01-3-3V4.5a3 3 0 116 0v8.25a3 3 0 01-3 3z"
+        />
+      </svg>
+    </button>
+  </div>
+</template>
+
+<script>
+import TheSearchButton from './TheSearchButton.vue';
+import TheSearchInput from './TheSearchInput.vue';
+
+export default {
+    components: {
+        TheSearchButton,
+        TheSearchInput
+    }
+};
+</script>
+
---
+++ src/components/TheSearchButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <button
+    class="flex items-center px-6 py-1 text-gray-600 bg-gray-100 border border-l-0 border-gray-300 rounded-tr-sm rounded-br-sm hover:bg-gray-200 focus:outline-none"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      class="w-5 h-5"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+      />
+    </svg>
+  </button>
+</template>
