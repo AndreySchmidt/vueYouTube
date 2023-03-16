4f6f195a48fc088cb0fa1da9d3d3d5bd071c3a45
AndreySchmidt <ringnull@gmail.com>
2023-03-16 10:55:27 +0300

    refactoring TheDropdownSettings

--- src/components/TheDropdownSettings.vue
+++ src/components/TheDropdownSettings.vue
@@ -5,6 +5,14 @@
     <section class="py-2 border-b border-gray-200">
       <ul>
         <DropDownSettingsListItem
+          v-for="item in list"
+          :key="item.label"
+          :label="item.label"
+          :icon="item.icon"
+          :with-sub-menu="item.withSubMenu"
+        />
+
+        <!-- <DropDownSettingsListItem
           label="Appearance: Light"
           icon="appearance"
           with-sub-menu
@@ -19,15 +27,12 @@
           icon="location"
           with-sub-menu
         />
-        <DropDownSettingsListItem label="Help" icon="help" with-sub-menu />
+        <DropDownSettingsListItem label="Help" icon="help" with-sub-menu /> -->
       </ul>
     </section>
     <section class="py-2">
       <ul>
-        <DropDownSettingsListItem
-          label="Restricted Mode: Off"
-          with-sub-menu
-        />
+        <DropDownSettingsListItem label="Restricted Mode: Off" with-sub-menu />
       </ul>
     </section>
   </div>
@@ -41,6 +46,32 @@ export default {
   components: {
     BaseIcon,
     DropDownSettingsListItem,
+  },
+  data() {
+    return {
+      list: [
+        {
+          label: "Appearance: Light",
+          icon: "appearance",
+          withSubMenu: true,
+        },
+        {
+          label: "Language: English",
+          icon: "language",
+          withSubMenu: true,
+        },
+        {
+          label: "Location: Russia",
+          icon: "location",
+          withSubMenu: true,
+        },
+        {
+          label: "Help",
+          icon: "help",
+          withSubMenu: true,
+        },
+      ],
+    };
   },
 };
 </script>
