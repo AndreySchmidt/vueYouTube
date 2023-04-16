d0169d5da09ee2bd7d0e51cff922bf973ac2332a
AndreySchmidt <ringnull@gmail.com>
2023-04-16 19:59:14 +0300

    thrumbnail

--- src/components/VideoItemThumbnail.vue
+++ src/components/VideoItemThumbnail.vue
@@ -2,16 +2,13 @@
   <a href="">
     <div class="relative">
       <img :src="`https://picsum.photos/seed/image${index}/720/404`" />
-      <span
-        class="bg-opacity-60 opacity-0 group-hover:opacity-100 absolute top-0 right-0 text-white bg-black rounded-sm m-1 p-1"
-      >
-        <BaseIcon name="clock" class="w-5 h-5" />
-      </span>
-      <span
+      <VideoItemThumbnailBtn icon="clock" label="Watch later" class="top-0" />
+      <!-- <span
         class="bg-opacity-60 opacity-0 group-hover:opacity-100 absolute top-8 right-0 text-white bg-black rounded-sm m-1 p-1"
       >
         <BaseIcon name="menuAlt3" class="w-5 h-5" />
-      </span>
+      </span> -->
+      <VideoItemThumbnailBtn icon="menuAlt3" label="Add to queque" class="top-8" />
       <span
         class="duration-500 opacity-100 group-hover:opacity-0 absolute bottom-0 right-0 text-white bg-black text-xs rounded-sm m-1 p-1 font-semibold"
         >05:45</span
@@ -22,10 +19,12 @@
 
 <script>
 import BaseIcon from "./BaseIcon.vue";
+import VideoItemThumbnailBtn from "./VideoItemThumbnailBtn.vue";
 
 export default {
   components: {
     BaseIcon,
+    VideoItemThumbnailBtn,
   },
 
   props: ["index"],
